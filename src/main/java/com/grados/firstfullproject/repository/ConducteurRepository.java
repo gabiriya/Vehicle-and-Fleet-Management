98b6975255400e1b2110dde5d52556d4edf47489
gabiriya <youssef.lotfi9@gmail.com>
2021-10-23 21:22:25 +0100

    Add repository annotation to repositories

--- src/main/java/com/grados/firstfullproject/repository/ConducteurRepository.java
+++ src/main/java/com/grados/firstfullproject/repository/ConducteurRepository.java
@@ -3,5 +3,8 @@ package com.grados.firstfullproject.repository;
 import com.grados.firstfullproject.entities.Conducteur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ConducteurRepository extends JpaRepository<Conducteur,Long> {
 }
--- src/main/java/com/grados/firstfullproject/repository/VehiculeRepository.java
+++ src/main/java/com/grados/firstfullproject/repository/VehiculeRepository.java
@@ -3,5 +3,8 @@ package com.grados.firstfullproject.repository;
 import com.grados.firstfullproject.entities.Vehicule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface VehiculeRepository extends JpaRepository<Vehicule,Long> {
 }
