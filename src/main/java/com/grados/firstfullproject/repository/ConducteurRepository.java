99902a05725f69fa792fb34ee1895879101231c2
gabiriya <youssef.lotfi9@gmail.com>
2021-11-03 08:37:35 +0100

    removing unused imports

--- src/main/java/com/grados/firstfullproject/repository/ConducteurRepository.java
+++ src/main/java/com/grados/firstfullproject/repository/ConducteurRepository.java
@@ -3,11 +3,8 @@ package com.grados.firstfullproject.repository;
 import com.grados.firstfullproject.entities.Conducteur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface ConducteurRepository extends JpaRepository<Conducteur,Long> {
