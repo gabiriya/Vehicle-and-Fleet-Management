f1e27f260845ed88e7a1cc60bedc319b97de7a8b
gabiriya <youssef.lotfi9@gmail.com>
2021-11-12 20:53:35 +0100

    added VehiculeMapper

--- src/main/java/com/grados/firstfullproject/mapper/VehiculeMapper.java
+++ src/main/java/com/grados/firstfullproject/mapper/VehiculeMapper.java
@@ -6,14 +6,14 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
-
-
 @Mapper(componentModel = "spring")
 public interface VehiculeMapper {
-
-    @Mapping(target = "dateExpAssurance", source = "assurance.dateExpiration")
-    @Mapping(target = "marque", source = "marque")
-    @Mapping(target = "assurance" , source = "assurance.nom")
+    @Mappings({
+            @Mapping(target = "assurance" , source = "assurance.nom"),
+            @Mapping(target = "dateExpAssurance", source = "assurance.dateExpiration"),
+            @Mapping(target = "marque", source = "marque")
+        }
+    )
 
     VehiculeDTO VehiculeToDto(Vehicule vehicule);
 }
