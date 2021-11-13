74d6a4bd8f22dd358d4a4dabd92e14421b45f1d0
gabiriya <youssef.lotfi9@gmail.com>
2021-11-13 12:40:18 +0100

    removed lombok from class

--- src/main/java/com/grados/firstfullproject/DTO/VehiculeDTO.java
+++ src/main/java/com/grados/firstfullproject/DTO/VehiculeDTO.java
@@ -1,15 +1,13 @@
 package com.grados.firstfullproject.DTO;
 
-import lombok.Data;
-
 import java.time.LocalDate;
 
-@Data
 public class VehiculeDTO {
-    private Long id;
-    private String marque;
-    private String assurance;
-    private LocalDate dateExpAssurance;
+    public Long id;
+    public String marque;
+    public String assurance;
+    public LocalDate dateExpAssurance;
+
 
 
 }
