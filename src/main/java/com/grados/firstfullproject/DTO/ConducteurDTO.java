4bb44a1d8692084a74973152a359b2075edce0ca
gabiriya <youssef.lotfi9@gmail.com>
2021-11-13 12:36:17 +0100

    created Driver Dto

---
+++ src/main/java/com/grados/firstfullproject/DTO/ConducteurDTO.java
@@ -0,0 +1,24 @@
+package com.grados.firstfullproject.DTO;
+
+
+public class ConducteurDTO {
+
+    private Long id;
+    private String fullName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
