bd86ae6a83ec9c1eb38c3a3c231a18a211d82318
gabiriya <youssef.lotfi9@gmail.com>
2021-11-13 12:43:54 +0100

    added lombok dependency to class

--- src/main/java/com/grados/firstfullproject/DTO/VehiculeDTO.java
+++ src/main/java/com/grados/firstfullproject/DTO/VehiculeDTO.java
@@ -1,13 +1,15 @@
 package com.grados.firstfullproject.DTO;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
 public class VehiculeDTO {
-    public Long id;
-    public String marque;
-    public String assurance;
-    public LocalDate dateExpAssurance;
-
+    private Long id;
+    private String marque;
+    private String assurance;
+    private LocalDate dateExpAssurance;
 
 
 }
