d0979da59aad82c1ef5e63c1d15360da6c9e58f2
gabiriya <youssef.lotfi9@gmail.com>
2022-03-03 13:18:47 +0100

    renamed methods and removed mapper

---
+++ src/main/java/com/grados/firstfullproject/mapper/DriverMapper.java
@@ -0,0 +1,21 @@
+package com.grados.firstfullproject.mapper;
+
+
+import com.grados.firstfullproject.DTO.DriverDTO;
+import com.grados.firstfullproject.entities.Driver;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface DriverMapper {
+
+    DriverDTO DriverToDTO(Driver driver);
+
+
+    Driver DtoToDriver(DriverDTO c);
+
+    List<Driver> dtosToDrivers(List<DriverDTO> driverDTOS);
+    List<DriverDTO> DriverssToDtos(List<Driver> drivers);
+}
