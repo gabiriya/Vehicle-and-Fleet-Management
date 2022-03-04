f1df356efd8ae7b9f362a97fce6df0bdb4ac94ce
gabiriya <youssef.lotfi9@gmail.com>
2022-03-04 15:19:02 +0100

    renamed fields and add driver param

--- src/main/java/com/grados/firstfullproject/controller/VehicleController.java
+++ src/main/java/com/grados/firstfullproject/controller/VehicleController.java
@@ -3,7 +3,7 @@ package com.grados.firstfullproject.controller;
 
 import com.grados.firstfullproject.DTO.VehicleDTO;
 import com.grados.firstfullproject.entities.Vehicle;
-import com.grados.firstfullproject.mapper.VehiculeMapper;
+import com.grados.firstfullproject.mapper.VehicleMapper;
 import com.grados.firstfullproject.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,49 +17,55 @@ import java.util.List;
 public class VehicleController {
 
     private final VehicleService vehiculeService;
-    private final VehiculeMapper vehiculeMapper;
+    private final VehicleMapper vehicleMapper;
 
     @Autowired
     public VehicleController(
             VehicleService vehiculeService,
-            VehiculeMapper vehiculeMapper
+            VehicleMapper vehicleMapper
     ) {
         this.vehiculeService = vehiculeService;
-        this.vehiculeMapper = vehiculeMapper;
+        this.vehicleMapper = vehicleMapper;
     }
 
-//    @PostMapping
-//    public ResponseEntity<Vehicule> saveVehicule(@RequestBody Vehicule vehicule){
-////        Long idAssurance  = vehicule.getAssurance().getId();
-////        Long idConducteur = vehicule.getConducteur().getId();
-//        return new ResponseEntity<>(vehiculeService.saveVehicule(vehicule,idAssurance,idConducteur), HttpStatus.CREATED);
-//    }
+    @PostMapping("/{idDriver}")
+    public ResponseEntity<Vehicle> saveVehicle(
+            @RequestBody Vehicle vehicle,
+            @PathVariable Long idDriver) {
+        return new ResponseEntity<>(vehiculeService.saveVehicle(vehicle,idDriver), HttpStatus.CREATED);
+    }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String > deleteVehicule(@PathVariable("id") Long id){
-        vehiculeService.deleteVehicle(id);
+    @DeleteMapping("/{idDriver}/{idVehicle}")
+    public ResponseEntity<String > deleteVehicle(
+            @PathVariable("idDriver") Long idDriver,
+            @PathVariable("idVehicle") Long idVehicle){
+        vehiculeService.deleteVehicle(idDriver,idVehicle);
         return new ResponseEntity<>("Vehicule deleted",HttpStatus.OK);
     }
 
-    @GetMapping("/all")
-    public List<Vehicle> getAllVehicules(){
-        return vehiculeService.findAllVehicles();
+    @GetMapping("{idDriver}")
+    public List<Vehicle> getAllVehicles(@PathVariable Long idDriver){
+        return vehiculeService.findAllVehicles(idDriver);
     }
 
     // get by id
 
-    @GetMapping("{id}")
-    public ResponseEntity<Vehicle> getVehiculeById(@PathVariable("id") Long id){
-        return new ResponseEntity<>(vehiculeService.getVehiculeById(id),HttpStatus.OK);
+    @GetMapping("{idDriver}/{idVehicle}")
+    public ResponseEntity<Vehicle> getVehicleById(
+            @PathVariable("idDriver") Long idDriver,
+            @PathVariable("idVehicle") Long idVehicle
+            ){
+        return new ResponseEntity<>(vehiculeService.getVehiculeById(idDriver,idVehicle),HttpStatus.OK);
     }
 
     // update
-    @PutMapping("{id}")
-    public ResponseEntity<Vehicle> updateVehicule(
-            @PathVariable("id") Long id,
-            @RequestBody Vehicle vehicule)
+    @PutMapping("/{idDriver}/{idVehicle}")
+    public ResponseEntity<Vehicle> updateVehicle(
+            @PathVariable("idVehicle") Long idVehicle,
+            @PathVariable("idDriver") Long idDriver,
+            @RequestBody Vehicle vehicle)
     {
-        return new ResponseEntity<>(vehiculeService.updateVehicle(vehicule,id),HttpStatus.OK);
+        return new ResponseEntity<>(vehiculeService.updateVehicle(idDriver,vehicle,idVehicle),HttpStatus.OK);
     }
 
     // get vehicule(dto)
@@ -69,10 +75,10 @@ public class VehicleController {
 //    }
 
    // get vehicule with conducteur and assurance and date exp assurance
-    @GetMapping("/car/{id}")
-    public VehicleDTO getVehiculeDto(@PathVariable("id") Long id){
-        return vehiculeMapper.VehiculeToDto(
-                vehiculeService.getVehiculeById(id)
-        );
-    }
+//    @GetMapping("/car/{id}")
+//    public VehicleDTO getVehiculeDto(@PathVariable("id") Long id){
+//        return vehicleMapper.VehiculeToDto(
+//                vehiculeService.getVehiculeById(id)
+//        );
+//    }
 }
