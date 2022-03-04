3884b0727994c26c5d122247fd1dd2bcde57c5bc
gabiriya <youssef.lotfi9@gmail.com>
2022-03-04 15:20:33 +0100

    added driver param

--- src/main/java/com/grados/firstfullproject/service/VehicleService.java
+++ src/main/java/com/grados/firstfullproject/service/VehicleService.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 public interface VehicleService {
 
-    Vehicle saveVehicle(Vehicle v,Long idConducteur);
-    List<Vehicle> findAllVehicles();
-    Vehicle getVehiculeById(Long id);
-    Vehicle updateVehicle(Vehicle v,Long id);
-    void deleteVehicle(Long id);
+    Vehicle saveVehicle(Vehicle v,Long idDriver);
+    List<Vehicle> findAllVehicles(Long idDriver);
+    Vehicle getVehiculeById(Long idDriver, Long idVehicle);
+    Vehicle updateVehicle( Long idDriver,Vehicle v,Long idVehicle);
+    void deleteVehicle(Long idDriver,Long idVehicle);
 //    List<CarDriversDTO> getAllCarDriver();
 //    CarDriversDTO carDriverEntityToDTO(Vehicule vehicule);
 }
