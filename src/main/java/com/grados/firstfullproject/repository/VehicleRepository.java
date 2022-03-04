8af9eda07f068fc8c8b229f6d9214cebf1ea5559
gabiriya <youssef.lotfi9@gmail.com>
2022-03-04 15:20:17 +0100

    added native queries

--- src/main/java/com/grados/firstfullproject/repository/VehicleRepository.java
+++ src/main/java/com/grados/firstfullproject/repository/VehicleRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
@@ -20,6 +21,16 @@ public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
 //    @Query("delete from Vehicle v where v.d.id = :id")
 //    void deleteAllVehiculeWithConduct(@Param("id") Long id);
 
-    @Query("select v from Vehicle v")
-    List<Vehicle> findAllVehicules();
+    @Query("select v from Vehicle v, Driver d where d.id = :idDriver ")
+    List<Vehicle> findAllVehicles(@Param("idDriver") Long idDriver);
+
+    @Query(value = "select v.* from vehicle v where v.id = :idVehicle and v.id_driver_fk = :idDriver",
+    nativeQuery = true)
+    Optional<Vehicle> findVehicleById(@Param("idDriver") Long idDriver);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM vehicle where id = :idVehicle and id_driver_fk = :idDriver",
+            nativeQuery = true)
+    void deleteVehicleById( @Param("idDriver") Long idDriver,@Param("idVehicle") Long idVehicle);
 }
